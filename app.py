85d291a6d263e8e138219a31479eb9d2d2a89bef
tquyen506 <105952719+tquyen506@users.noreply.github.com>
2026-08-06 09:29:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "status": "ok",
+        "service": "KTMT Import"
+    }
+
+
+@app.get("/health")
+def health():
+    return "running"
